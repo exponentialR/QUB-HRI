44990890133840faaa3d340c3917be1f9b70cebb
qub-hri <sadebayo01@qub.ac.uk>
2023-10-31 22:10:48 +0000

    rename first videos as calibration videos

--- calibration/setup_calib_direc.py
+++ calibration/setup_calib_direc.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from pathlib import Path
 import os
 from utils.logger_utils import setup_calibration_video_logger
+from shutil import move
 
 logger = setup_calibration_video_logger()
 
 
-def rearrange_pheo(parent_dir, start_participant, last_participant):
+def rearrange_pheo(parent_dir, start_participant, last_participant, cached_bin_path):
     """
     Renames video files in specific directories to 'CALIBRATION.MP4' based on certain conditions.
 
@@ -23,18 +24,20 @@ def rearrange_pheo(parent_dir, start_participant, last_participant):
     """
 
     # Get the current date and time for unique file naming
+    # Create the cached bin directory if it does not exist
+    os.makedirs('logs') if not os.path.exists('logs') else None
+    Path(cached_bin_path).mkdir(parents=True, exist_ok=True)
     current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
     # Create or open a log file to log renamed files
-    with open(f'{current_time}_renamed_files.log', 'w') as renamed_file:
+    with open(f'logs/{current_time}_renamed_files.log', 'w') as renamed_file:
         # Create or open a log file to log directories with more than 10 videos
-        with open(f'{current_time}_more_than_10_videos.log', 'w') as more_than_10_file:
+        with open(f'logs/{current_time}_more_than_10_videos.log', 'w') as more_than_10_file:
             # Write headers to log files
             renamed_file.write("Original File, Renamed File\n")
             more_than_10_file.write("Directory\n")
 
-            participant_list = [os.path.join(parent_dir, f'{participant_id:02d}') for participant_id in
-                                range(start_participant, last_participant + 1)]
+            participant_list = [f'p{id:02d}' for id in range(start_participant, last_participant+1)]
 
             # Loop through each participant folder
             for participant_id in participant_list:
@@ -48,6 +51,7 @@ def rearrange_pheo(parent_dir, start_participant, last_participant):
                 for camera_view in camera_views:
                     # List MP4 files in the current camera view folder
                     camera_view_videos = [i for i in sorted(os.listdir(camera_view)) if i.endswith('MP4')]
+                    THM_cached_files = [i for i in sorted(os.listdir(camera_view)) if i.endswith('THM')]
 
                     # Check if there are 10 or fewer MP4 files
                     if len(camera_view_videos) <= 10:
@@ -57,7 +61,8 @@ def rearrange_pheo(parent_dir, start_participant, last_participant):
                             pass
                         else:
                             # Generate full file paths for each video file
-                            camera_view_videos = [os.path.join(camera_view, video_file) for video_file in camera_view_videos]
+                            camera_view_videos = [os.path.join(camera_view, video_file) for video_file in
+                                                  camera_view_videos]
 
                             # Loop through each video file to rename it
                             for original_video_path in camera_view_videos:
@@ -73,14 +78,28 @@ def rearrange_pheo(parent_dir, start_participant, last_participant):
                                         renamed_file.write(f"{original_video_path}, {new_video_filepath}\n")
 
                                     else:
-                                        print(f"A file already exists at {new_video_filepath}. Rename operation aborted.")
+                                        print(
+                                            f"A file already exists at {new_video_filepath}. Rename operation aborted.")
                                 else:
                                     print(f"The file {original_video_path} does not exist.")
                     else:
                         logger.critical(f'Video Files more than 10 Please check {camera_view}')
                         more_than_10_file.write(f"{camera_view}\n")
+                    for thm_file in THM_cached_files:
+                        original_thm_path = os.path.join(camera_view, thm_file)
+                        if os.path.exists(thm_file):
+
+                            print(f'Original ThM path: {original_thm_path}')
+                            new_thm_filepath = os.path.join(cached_bin_path, participant_id, os.path.basename(camera_view), thm_file)
+
+                            Path(os.path.dirname(new_thm_filepath)).mkdir(parents=True, exist_ok=True)
+                            move(original_thm_path, new_thm_filepath)
+                            logger.info(f'Moved {original_thm_path} to {new_thm_filepath}')
+                        else:
+                            logger.info(f'{original_thm_path} May already be moved as it is not found in CWD')
 
 
 if __name__ == '__main__':
-    parent_dir = ''
-    rearrange_pheo(parent_dir, 1, 10)
+    parent_dir = '/home/qub-hri/Documents/PHEO Waiting Data'
+    cached_bin = '/home/qub-hri/Documents/PHEO Waiting Data/Cached-THM'
+    rearrange_pheo(parent_dir, 1, 10, cached_bin)
