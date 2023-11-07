b9a5f0723fc5ad16895a9370f3b2a469e5222e2a
Samuel Adebayo <samueladebayo@ieee.org>
2023-11-07 17:38:31 +0000

    updated

--- calibration/setup_calib_direc.py
+++ calibration/setup_calib_direc.py
@@ -113,4 +113,4 @@ def rearrange_pheo(parent_dir, start_participant, last_participant, cached_bin_p
 if __name__ == '__main__':
     parent_dir = '/home/iamshri/Documents/PHEO-Data'
     cached_bin = '/home/iamshri/Documents/PHEO-Data/Cached-THM'
-    rearrange_pheo(parent_dir, 21, 30, cached_bin)
+    rearrange_pheo(parent_dir, 31, 40, cached_bin)
