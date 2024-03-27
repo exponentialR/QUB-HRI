ba89754c7ac8dfa1c2c2b007874c82ed86b38634
Samuel Adebayo <sadebayo01@qub.ac.uk>
2024-03-27 13:10:44 +0000

    test displaying synchronized video

--- reconstruction/extract_landmark.py
+++ reconstruction/extract_landmark.py
@@ -6,17 +6,14 @@ import cv2
 from tqdm import tqdm
 import mediapipe as mp
 
-from utils import load_calibration_data, get_video_fps, extract_landmarks, calculate_gaze_vector
+from utils import get_video_fps, extract_landmarks
 
-class ExtractData:
-    def __init__(self, left_video_path, right_video_path, left_path_h5=None, right_path_h5=None, fps=None, max_hands=2):
 
-        self.left_video_path = left_video_path
-        self.right_video_path = right_video_path
-        self.left_path_hd5 = left_path_h5
-        self.right_path_hd5 = right_path_h5
-
-        # Lod participant video calibration data
+class ExtractLandmark:
+    def __init__(self, image, cam_matrix, dist_coeffs, path_h5=None, max_hands=2):
+        self.image = image
+        self.path_hd5 = path_h5
+        self.cam_matrix, self.dist_coeffs = cam_matrix, dist_coeffs
         self.face_mesh = mp.solutions.face_mesh.FaceMesh(min_tracking_confidence=.5, min_detection_confidence=.5,
                                                          refine_landmarks=True)
         self.hands = mp.solutions.hands.Hands(min_detection_confidence=.5, min_tracking_confidence=.5,
@@ -27,12 +24,6 @@ class ExtractData:
         self.left_iris_indices = [468, 469, 470, 471, 472]
         self.right_iris_indices = [473, 474, 475, 476, 477]
 
-        if fps is None:
-            self.left_fps = get_video_fps(self.left_video_path)
-            self.right_fps = get_video_fps(self.right_video_path)
-        else:
-            self.fps = fps
-
     def process_frame(self, frame):
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
@@ -41,229 +32,55 @@ class ExtractData:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         return results, image
 
-    def process_head_gaze_face(self, left_frame, right_frame, left_frame_index, right_frame_index):
-        left_image, left_results = self.process_frame(left_frame)
-        right_frame, right_results = self.process_frame(right_frame)
-
-        left_timestamp = left_frame_index / self.fps
-        right_timestamp = right_frame_index / self.fps
-        if results.multi_face_landmarks:
-            face_landmarks = results.multi_face_landmarks[0]
-            head_eye_data = self.calculate_head_pose(image, face_landmarks)
-            gaze_vector = calculate_gaze_vector(head_eye_data['li3d'], head_eye_data['ri3d'], head_eye_data['f3d'],
-                                                head_eye_data['rvec'])
-            facial_landmarks = extract_landmarks(face_landmarks)
-
-            # Convert NumPy arrays in head_eye_data to lists
-            for key in ['f3d', 'f2d', 'rvec', 'tvec', 'li3d', 'ri3d', 'cam_matrix', 'dist_matrix']:
-                if key in head_eye_data and isinstance(head_eye_data[key], np.ndarray):
-                    head_eye_data[key] = head_eye_data[key].tolist()
-
-            # Convert gaze_vector and facial_landmarks if they are NumPy arrays
-            if isinstance(gaze_vector, np.ndarray):
-                gaze_vector = gaze_vector.tolist()
-            if isinstance(facial_landmarks, np.ndarray):
-                facial_landmarks = facial_landmarks.tolist()
-            face_present = 1
-            data = {
-                'timestamp': timestamp,
-                'frame_index': frame_index,
-                'headpose_data': head_eye_data,
-                'gaze_2d': gaze_vector,
-                'face_landmarks': facial_landmarks,
-                'face_landmark_presence': face_present
-            }
-
-        else:  # in case there is no face landmark detected
-            cam_matrix = np.array([[1, 0, 0],
-                                   [0, 1, 0],
-                                   [0, 0, 1]])
-            dist_matrix = np.zeros((4, 1), dtype=np.float64)
-            hand_eye_data = {
-                'head_pitch': 0, 'head_yaw': 0, 'head_roll': 0,
-                'f3d': (0, 0, 0), 'f2d': (0, 0),
-                'rvec': (0, 0, 0), 'tvec': (0, 0, 0),
-                'cam_matrix': cam_matrix,
-                'dist_matrix': dist_matrix,
-                'li3d': (0, 0, 0), 'ri3d': (0, 0, 0),
-                'text': 'Head-Eye'
-            }
-            gaze_vector = (0, 0, 0)
-            facial_landmarks = [(0.0, 0.0, 0.0)] * 13
-            face_present = 0
-            data = {'timestamp': timestamp,
-                    'frame_index': frame_index,
-                    'headpose_data': hand_eye_data,
-                    'gaze_2d': gaze_vector,
-                    'face_landmarks': facial_landmarks,
-                    'face_landmark_presence': face_present
-                    }
-        return data
-
-    def calculate_head_pose(self, left_image, right_image, left_face_landmarks, right_face_landmarks):
+    def calculate_head_pose(self, processed_image, face_landmarks):
         """
         Given an image and corresponding facial landmark,
-        this method extracts head pose [pitch, yaw, and roll], left and right iris in 3D
-        :param image: RGB image
+        this method extracts head pose [pitch, yaw, and roll], Iris in 3D and 2D, and forehead in 3D and 2D
+        :param processed_image: Mediapipe processed image
         :param face_landmarks: 473 facial landmarks
         :return:
         """
-        left_image_height, left_image_width, _ = left_image.shape
-        right_image_height, right_image_width, _ = right_image.shape
-
-        face_3d_left, face_2d_left = [], []
-        face_3d_right, face_2d_right = [], []
-        left_left_iris_3d, left_right_iris_3d = None, None
-        right_left_iris_3d, right_right_iris_3d = None, None
-
-        left_forehead_2d, left_forehead_3d = None, None
-        right_forehead_2d, right_forehead_3d = None, None
+        processed_image_height, processed_image_width, _ = processed_image.shape
+        face, face_2d = [], []
+        left_iris, right_iris = None, None
+        forehead = None
 
         for idx, lm in enumerate(face_landmarks.landmark):
             if idx in self.head_pose_indices:
-                x, y = int(lm.x * img_width), int(lm.y * img_height)
+                x, y = int(lm.x * processed_image_width), int(lm.y * processed_image_height)
+                face.append([x, y, lm.z])
                 face_2d.append([x, y])
-                face_3d.append([x, y, lm.z])
             if idx == 6:
-                forehead_2d = (lm.x * img_width, lm.y * img_height)
-                forehead_3d = (lm.x * img_width, lm.y * img_height, lm.z)
+                forehead = (lm.x * processed_image_width, lm.y * processed_image_height, int(lm.z))
             if idx == 468:
-                left_iris_3d = (int(lm.x * img_width), int(lm.y * img_height), int(lm.z))
+                left_iris = (int(lm.x * processed_image_width), int(lm.y * processed_image_height), int(lm.z))
             if idx == 473:
-                right_iris_3d = (int(lm.x * img_width), int(lm.y * img_height), int(lm.z * 3000))
+                right_iris = (int(lm.x * processed_image_width), int(lm.y * processed_image_height), int(lm.z))
 
-        face_2d = np.array(face_2d, dtype=np.float64)
-        face_3d = np.array(face_3d, dtype=np.float64)
+        face_extracted = np.array(face, dtype=np.float64)
+        face_2d = np.array(face, dtype=np.float64)
 
-        if self.part_cam_matrix is not None and self.part_dist_coeffs is not None:
-            cam_matrix = self.part_cam_matrix
-            dist_matrix = self.part_dist_coeffs
+        if self.cam_matrix is not None and self.dist_coeffs is not None:
+            cam_matrix = self.cam_matrix
+            dist_matrix = self.dist_coeffs
         else:
-            focal_length = 1 * img_width
-            cam_matrix = np.array([[focal_length, 0, img_height / 2],
-                                   [0, focal_length, img_width / 2],
+            focal_length = 1 * processed_image_width
+            cam_matrix = np.array([[focal_length, 0, processed_image_height / 2],
+                                   [0, focal_length, processed_image_width / 2],
                                    [0, 0, 1]])
             dist_matrix = np.zeros((4, 1), dtype=np.float64)
-        success, rvec, tvec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+        success, rvec, tvec = cv2.solvePnP(face_extracted, face_2d, cam_matrix, dist_matrix)
         rmat, jac = cv2.Rodrigues(rvec)
         euler_angles = cv2.decomposeProjectionMatrix(rmat)[6]
         pitch, yaw, roll = euler_angles
-        text = 'Head-Eye'
+        text = 'HeadEyeData'
         head_eye = {
-            # 'x': x, 'y': y, 'z': z,
-            'head_pitch': pitch, 'head_yaw': yaw, 'head_roll': roll,
-            'f3d': forehead_3d, 'f2d': forehead_2d,
+            'head_pitch': pitch, 'head_yaw': yaw, 'head_roll': roll, 'forehead': forehead,
             'rvec': rvec, 'tvec': tvec,
             'cam_matrix': cam_matrix,
             'dist_matrix': dist_matrix,
-            'li3d': left_iris_3d, 'ri3d': right_iris_3d,
+            'left_iris': left_iris, 'right_iris': right_iris,
             'text': text
         }
         return head_eye
 
-    def process_hand_landmarks(self, frame):
-        results = self.hands.process(frame)
-        hand_landmarks_list = []
-
-        if results.multi_hand_landmarks:
-            # Extract landmarks for the first detected hand
-            for lm in results.multi_hand_landmarks[0].landmark:
-                hand_landmarks_list.append((lm.x, lm.y, lm.z))
-        else:
-            # Append zeros for each of the 21 landmarks if no hand is detected
-            hand_landmarks_list = [(0.0, 0.0, 0.0) for _ in range(21)]
-
-        if isinstance(hand_landmarks_list, np.ndarray):
-            hand_landmarks_list = hand_landmarks_list.tolist()
-
-        return hand_landmarks_list
-
-    def process_upper_body_pose(self, frame):
-        upper_body_landmarks = []
-        results = self.pose.process(frame)
-
-        if results.pose_landmarks:
-            for idx, landmark in enumerate(results.pose_landmarks.landmark):
-                if idx in range(11, 24):
-                    upper_body_landmarks.append((landmark.x, landmark.y, landmark.z))
-        else:
-            # If no landmarks detected, fill with zeros
-            upper_body_landmarks = [(0.0, 0.0, 0.0)] * 13
-        return upper_body_landmarks
-
-    def extract_data(self):
-
-        left_cap_participant = cv2.VideoCapture(self.left_video_path)
-        left_video_directory = os.path.dirname(self.left_video_path)
-        left_file_name = os.path.join(left_video_directory,
-                                      os.path.splitext(os.path.basename(self.left_video_path))[0] + '.h5')
-        # TODO:
-
-        total_frames = int(left_cap_participant.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        with h5py.File(left_file_name, 'w') as hdf_file:
-            pbar = tqdm(total=total_frames, desc='Extracting Landmarks from Video', unit='frame')
-            timestamps, frame_indices, gaze_2d, face_landmarks, hand_landmarks, upper_body_pose, face_presence = [], [], [], [], [], [], []
-            headpose_data = {key: [] for key in
-                             ['head_pitch', 'head_yaw', 'head_roll', 'f3d', 'f2d', 'rvec', 'tvec', 'cam_matrix',
-                              'dist_matrix', 'li3d', 'ri3d']}
-
-            while left_cap_participant.isOpened():
-                ret, frame = left_cap_participant.read()
-                if not ret:
-                    break
-                try:
-                    data = self.process_head_gaze_face(frame, pbar.n)
-                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    image.flags.writeable = False
-
-                    hand_landmark = self.process_hand_landmarks(image)
-                    upper_body_landmarks = self.process_upper_body_pose(image)
-
-                    image.flags.writeable = True
-                    # if data:
-                    timestamps.append(data['timestamp'])
-                    frame_indices.append(data['frame_index'])
-                    for key in headpose_data:
-                        headpose_data[key].append(data['headpose_data'][key])
-                    gaze_2d.append(data['gaze_2d'])
-                    face_landmarks.append(data['face_landmarks'])
-
-                    # hand_landmarks.append(data['hand_landmarks'])
-                    upper_body_pose.append(upper_body_landmarks)
-                    hand_landmarks.append(hand_landmark)
-                    face_presence.append(data['face_landmark_presence'])
-
-                except Exception as e:
-                    print(f"Error processing frame {pbar.n}: {e}")
-                pbar.update(1)
-
-            left_cap_participant.release()
-
-            # Save data to HDF5
-            hdf_file.create_dataset('timestamps', data=np.array(timestamps))
-            hdf_file.create_dataset('face_landmark_presence', data=np.array(face_presence))
-            hdf_file.create_dataset('frame_indices', data=np.array(frame_indices))
-            for key, values in headpose_data.items():
-                hdf_file.create_dataset(f'headpose_data/{key}', data=np.array(values))
-            hdf_file.create_dataset('gaze_2d', data=np.array(gaze_2d))
-            hdf_file.create_dataset('face_landmarks', data=np.array(face_landmarks))
-            hdf_file.create_dataset('upper_body_pose', data=np.array(upper_body_pose))
-            hdf_file.create_dataset('hand_landmarks', data=np.array(hand_landmarks))
-
-    def read_h5_file(self):
-        with h5py.File(self.left_path_hd5, 'r') as hdf_file:
-            timestamps = hdf_file['timestamps'][:]
-            timestamps_list = timestamps.tolist()
-            print(f'Length of timestamps: {len(timestamps_list)}')
-            data = {}
-            for key in hdf_file.keys():
-                if isinstance(hdf_file[key], h5py.Group):
-                    group_data = {}
-                    for subkey in hdf_file[key].keys():
-                        group_data[subkey] = hdf_file[key][subkey][:]
-                    data[key] = group_data
-                elif isinstance(hdf_file[key], h5py.Dataset):
-                    data[key] = hdf_file[key][:]
-
-        return data
---
+++ reconstruction/test-ground/synchronized_video.py
@@ -0,0 +1,153 @@
+import cv2
+import numpy as np
+import os
+
+
+def frame_difference(left_video, right_video):
+    """
+    Sunchronize two videos by skipping the first few frames of the longer video.
+    Then Visualize them size by side.
+    :param left_video: str: Path to the left video
+    :param right_video: str: Path to the right video
+    return: start_frame: int: The frame to start reading for the longer video and which video is longer
+    """
+    left_cap = cv2.VideoCapture(left_video)
+    right_cap = cv2.VideoCapture(right_video)
+    left_frame_count = int(left_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    right_frame_count = int(right_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count_difference = abs(left_frame_count - right_frame_count)
+    print(f'There are {frame_count_difference} frames difference between the two videos')
+    print(
+        f'attempting to synchronize videos {os.path.basename(left_video)} and {os.path.basename(right_video)}')
+    print(f'Left video frame count: {left_frame_count}')
+    print(f'Right video frame count: {right_frame_count}')
+    if left_frame_count > right_frame_count:
+        print(f'Skip frame {frame_count_difference} in left video')
+        longer_video = 'left'
+    elif right_frame_count > left_frame_count:
+        print(f'Skip frame {frame_count_difference} in right video')
+        longer_video = 'right'
+    else:
+        longer_video = 'none'
+    return frame_count_difference, longer_video
+
+
+def display_synchronized_video(left_video, right_video, frame_difference, longer_video):
+    """
+    Display the synchronized videos side by side by starting from the start_frame of the longer video.
+    :param left_video: str: Path to the left video
+    :param right_video: str: Path to the right video
+    :param frame_difference: int: The frame to start reading for the longer video
+    :param longer_video: str: The longer video
+    :return: None
+    """
+    print(frame_difference, longer_video)
+    left_cap = cv2.VideoCapture(left_video)
+    right_cap = cv2.VideoCapture(right_video)
+
+    # Set the starting frame for the longer video
+    if longer_video == 'left':
+        print(f'Skipping {frame_difference} frames in left video')
+        for _ in range(frame_difference):
+            left_cap.read()  # Skip initial frames in left video
+        # left_cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    elif longer_video == 'right':
+        print(f'Skipping {frame_difference} frames in right video')
+        for _ in range(frame_difference):
+            right_cap.read()  # Skip initial frames in right video
+        # right_cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    left_fps = left_cap.get(cv2.CAP_PROP_FPS)
+    right_fps = right_cap.get(cv2.CAP_PROP_FPS)
+    common_fps = min(left_fps, right_fps)
+
+    while True:
+        left_time = left_cap.get(cv2.CAP_PROP_POS_MSEC)
+        right_time = right_cap.get(cv2.CAP_PROP_POS_MSEC)
+        time_difference = abs(left_time - right_time)
+
+        if time_difference > (1000 / common_fps):
+            if left_time < right_time:
+                left_cap.read()
+            else:
+                right_cap.read()
+            continue
+        ret_left, frame_left = left_cap.read()
+        ret_right, frame_right = right_cap.read()
+
+        # Break the loop if either video ends
+        if not ret_left or not ret_right:
+            print("Reached the end of one or both videos.")
+            break
+
+        # Resize the frames to have the same height
+        height_left, width_left = frame_left.shape[:2]
+        height_right, width_right = frame_right.shape[:2]
+        height = 920  # min(height_left, height_right)  # Use the smaller height for both frames
+        frame_left = cv2.resize(frame_left, (int(width_left * (height / height_left)), height))
+        frame_right = cv2.resize(frame_right, (int(width_right * (height / height_right)), height))
+
+        # Combine the frames side by side
+        combined_frame = np.hstack((frame_left, frame_right))
+
+        # Display the combined frame
+        cv2.imshow('Synchronized Videos', combined_frame)
+
+        # Break the loop on pressing 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture objects and close all windows
+    left_cap.release()
+    right_cap.release()
+    cv2.destroyAllWindows()
+
+
+def display_video(left_video, right_video):
+    """
+    Display the synchronized videos side by side by starting from the start_frame of the longer video.
+    :param left_video: str: Path to the left video
+    :param right_video: str: Path to the right video
+    :return: None
+    """
+    left_cap = cv2.VideoCapture(left_video)
+    right_cap = cv2.VideoCapture(right_video)
+
+    while True:
+        ret_left, frame_left = left_cap.read()
+        ret_right, frame_right = right_cap.read()
+
+        # Break the loop if either video ends
+        if not ret_left or not ret_right:
+            print("Reached the end of one or both videos.")
+            break
+
+        # Resize the frames to have the same height
+        height_left, width_left = frame_left.shape[:2]
+        height_right, width_right = frame_right.shape[:2]
+        height = 920  # min(height_left, height_right)  # Use the smaller height for both frames
+        frame_left = cv2.resize(frame_left, (int(width_left * (height / height_left)), height))
+        frame_right = cv2.resize(frame_right, (int(width_right * (height / height_right)), height))
+
+        # Combine the frames side by side
+        combined_frame = np.hstack((frame_left, frame_right))
+
+        # Display the combined frame
+        cv2.imshow('Synchronized Videos', combined_frame)
+
+        # Break the loop on pressing 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture objects and close all windows
+    left_cap.release()
+    right_cap.release()
+    cv2.destroyAllWindows()
+
+
+# Example usage
+left_video_path = '/home/iamshri/Documents/Test-Video/p03/CAM_LL/CALIBRATION_CC.MP4'
+right_video_path = '/home/iamshri/Documents/Test-Video/p03/CAM_LR/CALIBRATION_CC.MP4'
+start_frame, longer_video = frame_difference(left_video_path, right_video_path)
+print(f' Start Frame: {start_frame}', f' Longer Video: {longer_video}')
+display_synchronized_video(left_video_path, right_video_path, start_frame, longer_video)
+# display_video(left_video_path, right_video_path)
