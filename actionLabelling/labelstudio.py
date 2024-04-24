1e2319150ff4734a17c6b7091b2fbb3c45f93993
qub-hri <sadebayo01@qub.ac.uk>
2024-04-24 14:23:13 +0100

    label-studio application run

---
+++ actionLabelling/labelstudio.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import subprocess
+from tkinter import font
+
+
+def start_label_studio():
+    # Use subprocess to call the shell script
+    subprocess.Popen(['/home/qub-hri/PycharmProjects/QUB-HRI/actionLabelling/label_studio.sh'], shell=True)
+
+def stop_label_studio():
+    # Kill all instances of Label Studio
+    subprocess.run(['pkill', '-f', 'label-studio'])
+
+# Create the main window
+root = tk.Tk()
+root.title("Label Studio Manager")
+root.geometry('300x200')  # Width x Height
+root.config(bg='light blue')  # Background color of the window
+
+# Custom font
+customFont = font.Font(family="Helvetica", size=12, weight="bold")
+
+# Add buttons with colors
+start_button = tk.Button(root, text="Start Label Studio", command=start_label_studio, bg='green', fg='white', font=customFont)
+start_button.pack(pady=20, fill=tk.X, padx=20)
+
+stop_button = tk.Button(root, text="Stop Label Studio", command=stop_label_studio, bg='red', fg='white', font=customFont)
+stop_button.pack(pady=20, fill=tk.X, padx=20)
+
+# Run the GUI
+root.mainloop()
