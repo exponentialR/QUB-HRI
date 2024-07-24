aaa1fa319ce1311cd131772d5412d46bdd26a77b
samueladebayo <samueladebayo@ieee.org>
2024-07-24 13:06:20 +0100

    triangulate views

--- preprocessing/segment_videos.py
+++ preprocessing/segment_videos.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import shutil
 import subprocess
 from tqdm import tqdm
 
@@ -22,7 +23,8 @@ def open_json_file(json_path):
 def segment_videos(json_files_directory, base_videos_directory, output_directory):
     json_list = [file for file in os.listdir(json_files_directory) if file.endswith('.json') and file.startswith(TASKS)]
     print('LENGTH OF JSON LIST: ', len(json_list))
-
+    skipped_files = []
+    skipped_view = {view: 0 for view in CAM_VIEWS}
     for filename in tqdm(json_list, desc='Processing JSON files'):
         json_path = os.path.join(json_files_directory, filename)
         data = open_json_file(json_path)
@@ -33,31 +35,42 @@ def segment_videos(json_files_directory, base_videos_directory, output_directory
                 for trick in tqdm(task['tricks'], desc=f'Processing tricks'):
                     label_abv = extract_bracket_letters(trick['labels'][0])
                     label_dir = os.path.join(output_directory, label_abv)
-                    os.makedirs(label_dir, exist_ok=True)  # Ensure the directory exists
+                    # os.makedirs(label_dir, exist_ok=True)  # Ensure the directory exists
 
                     start_time = str(trick['start'])
                     end_time = str(trick['end'])
                     output_filename = f'{vid_url[0]}-{cam_view}-{vid_url[3]}_{vid_url[-1][0:-4]}-{label_abv}-{start_time}_{end_time}.mp4'
                     output_path = os.path.join(label_dir, output_filename)
+                    if os.path.exists(output_path) and os.path.getsize(output_path) < 100000:
+                        # shutil.move(output_path, os.path.join(corrupted_seg_dir, output_filename))
+                        print(f'Moved corrupted file {output_path} to {corrupted_seg_dir}')
+                        skipped_view[cam_view] += 1
+                        skipped_files.append(output_path)
 
-                    vid_path = os.path.join(base_videos_directory, vid_url[0],
-                                            f'{vid_url[1]}_{vid_url[2]}/{vid_url[3]}_{vid_url[4]}')
 
-                    if os.path.exists(vid_path):
-                        command = [
-                            'ffmpeg', '-i', vid_path, '-ss', start_time, '-to', end_time, '-c', 'copy',
-                            '-n',
-                            output_path
-                        ]
-                        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                        print(f'Segmented video saved at {output_path}')
-                    else:
-                        continue
+                    # vid_path = os.path.join(base_videos_directory, vid_url[0],
+                    #                         f'{vid_url[1]}_{vid_url[2]}/{vid_url[3]}_{vid_url[4]}')
+
+                    # if os.path.exists(vid_path):
+                    #     command = [
+                    #         'ffmpeg', '-i', vid_path, '-ss', start_time, '-to', end_time, '-c', 'copy',
+                    #         '-n',
+                    #         output_path
+                    #     ]
+                    #     # subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    #     print(f'Segmented video saved at {output_path}')
+                    # else:
+                    #     continue
+    print('Skipped files: ', skipped_files)
+    print('Total skipped files: ', len(skipped_files))
+    print('Skipped views: ', skipped_view)
 
 
 if __name__ == '__main__':
     json_files_directory = '/home/samueladebayo/PycharmProjects/QUB-HRI/actionLabelling/labels'
     base_videos_directory = '/media/samueladebayo/EXTERNAL_USB/QUB-PHEO-Proceesed'
-    output_directory = '/home/samueladebayo/Documents/PhD/QUB-PHEO-Dataset/segmented_videos'
+    output_directory = '/home/samueladebayo/Documents/PhD/QUB-PHEO-Dataset/Annotated'
     os.makedirs(output_directory, exist_ok=True)
+    corrupted_seg_dir = '/home/samueladebayo/Documents/PhD/QUB-PHEO-Dataset/corrupted-segment'
     segment_videos(json_files_directory, base_videos_directory, output_directory)
+
--- reconstruction/triangulate_views.py
+++ reconstruction/triangulate_views.py
@@ -1,77 +1,81 @@
-import cv2
-import h5py
 import numpy as np
-import os
-import logging
-from tqdm import tqdm
 import h5py
+import cv2
+
+
+def load_keypoints(hdf5_file):
+    with h5py.File(hdf5_file, 'r') as f:
+        face_landmarks = f['face_landmarks'][:]  # shape (923, 478, 2)
+        left_hand_landmarks = f['left_hand_landmarks'][:]  # shape (923, 21, 2)
+        pose_landmarks = f['pose_landmarks'][:]  # shape (923, 33, 2)
+        right_hand_landmarks = f['right_hand_landmarks'][:]  # shape (923, 21, 2)
+    return face_landmarks, left_hand_landmarks, pose_landmarks, right_hand_landmarks
+
 
 class TriangulateViews:
-    def __init__(self, l_hdf5_path, r_hdf5_path, o_hdf5_path, logger=None, stereo_calib=None):
-        self.left_hdf5_path = l_hdf5_path
-        self.right_hdf5_path = r_hdf5_path
-        self.output_hdf5_path = o_hdf5_path
-        self.logger = logger
-        self.datasets = ['face_landmarks', 'pose_landmarks', 'left_hand_landmarks', 'right_hand_landmarks']
-        if stereo_calib is not None:    # Load the stereo calibration data
-            self.steeo_calib = np.load(stereo_calib)
+    def __init__(self, stereo_path, left_path, right_path, output_file):
+        self.stereo_calib = np.load(stereo_path)
+        self.left_path = left_path
+        self.right_path = right_path
+        self.R = self.stereo_calib['R']
+        self.T = self.stereo_calib['T']
+        self.P1 = np.dot(self.stereo_calib['left_mtx'], np.hstack((np.eye(3), np.zeros((3, 1)))))
+        self.P2 = np.dot(self.stereo_calib['right_mtx'], np.hstack((self.R, self.T.reshape(3, 1))))
+        self.face_landmarks_left, self.left_hand_landmarks_left, self.pose_landmarks_left, self.right_hand_landmarks_left = load_keypoints(
+            left_path)
+        self.face_landmarks_right, self.left_hand_landmarks_right, self.pose_landmarks_right, self.right_hand_landmarks_right = load_keypoints(
+            right_path)
+        self.output_file = output_file
 
-            # Load Claibration data
-            self.T = self.stereo_data['T']
-            self.R = self.stereo_data['R']
+    def triangulate_points(self, pts_left, pts_right):
+        points_4d_hom = cv2.triangulatePoints(self.P1, self.P2, pts_left, pts_right)
+        points_4d = points_4d_hom / points_4d_hom[3]
+        return points_4d[:3].T  # Return only the 3D points
 
-    def load_keypoints(self, hdf_path):
-        with h5py.File(hdf_path, 'r') as f:
-            keypoints = {key: f[key][:] for key in f.keys()}
-        return keypoints
+    def get_3d_points_for_frame(self, frame_number):
+        pts_left_face = self.face_landmarks_left[frame_number].reshape(-1, 2).T
+        pts_right_face = self.face_landmarks_right[frame_number].reshape(-1, 2).T
+        points_3d_face = self.triangulate_points(pts_left_face, pts_right_face)
 
-    def load_selected_datasets(self, file_path):
-        hdf5_file = h5py.File(file_path, 'r')
-        selected_data = {}
-        for dataset in self.datasets:
-            if dataset in hdf5_file.keys():
-                selected_data[dataset] = hdf5_file[dataset]
-            else:
-                self.logger.error(f'Dataset {dataset} not found in {file_path}')
-        return selected_data
+        pts_left_left_hand = self.left_hand_landmarks_left[frame_number].reshape(-1, 2).T
+        pts_right_left_hand = self.left_hand_landmarks_right[frame_number].reshape(-1, 2).T
+        points_3d_left_hand = self.triangulate_points(pts_left_left_hand, pts_right_left_hand)
 
-    def compute_disparity(self):
-        pass
+        pts_left_right_hand = self.right_hand_landmarks_left[frame_number].reshape(-1, 2).T
+        pts_right_right_hand = self.right_hand_landmarks_right[frame_number].reshape(-1, 2).T
+        points_3d_right_hand = self.triangulate_points(pts_left_right_hand, pts_right_right_hand)
 
-    def calculate_depth(self):
-        pass
+        pts_left_pose = self.pose_landmarks_left[frame_number].reshape(-1, 2).T
+        pts_right_pose = self.pose_landmarks_right[frame_number].reshape(-1, 2).T
+        points_3d_pose = self.triangulate_points(pts_left_pose, pts_right_pose)
 
-    def convert_3D_coordinates(self):
-        pass
+        return points_3d_face, points_3d_left_hand, points_3d_right_hand, points_3d_pose
 
-    def transform_coordinates(self):
-        pass
+    def save_3d_points_hdf5(self):
+        num_frames = self.face_landmarks_left.shape[0]
+        with h5py.File(self.output_file, 'w') as f:
+            face_points_3d = f.create_dataset('face_points_3d', (num_frames, 478, 3), dtype='f')
+            left_hand_points_3d = f.create_dataset('left_hand_points_3d', (num_frames, 21, 3), dtype='f')
+            right_hand_points_3d = f.create_dataset('right_hand_points_3d', (num_frames, 21, 3), dtype='f')
+            pose_points_3d = f.create_dataset('pose_points_3d', (num_frames, 33, 3), dtype='f')
 
-    def triangulate(self):
-        # load the left hdf5 file
-        left_hdf5_file = h5py.File(self.left_hdf5_path, 'r')
-        right_hdf5_file = h5py.File(self.right_hdf5_path, 'r')
-        # Ensure the keypoints have same length in both files
-        assert len(left_hdf5_file.keys()) == len(right_hdf5_file.keys()), "Mismatch in number of keypoints."
-        print(f'Number of keypoints: {len(left_hdf5_file.keys())}')
+            for frame in range(num_frames):
+                points_3d_face, points_3d_left_hand, points_3d_right_hand, points_3d_pose = self.get_3d_points_for_frame(
+                    frame)
+                face_points_3d[frame] = points_3d_face
+                left_hand_points_3d[frame] = points_3d_left_hand
+                right_hand_points_3d[frame] = points_3d_right_hand
+                pose_points_3d[frame] = points_3d_pose
 
-        # for key, value in left_hdf5_file.items():
-        #     left_data_shape = value.shape
-        #     right_data_shape = right_hdf5_file[key].shape
-        #     if left_data_shape != right_data_shape:
-        #         self.logger.error(f'Shape mismatch between left and right datasets for {key}')
-        #         return
-        #     self.logger.info(f'Shape of {key} dataset: {left_data_shape}')
-        # print(f'Frame Length: {left_data_shape[0]}')
+        print(f"3D points saved to {self.output_file}")
 
 
+# Test the TriangulateViews class
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    stereo_path = 'stereo_calib.npz'
+    left_path = 'left_keypoints.hdf5'
+    right_path = 'right_keypoints.hdf5'
+    output_file = '3d_points.hdf5'
 
-    left_hdf5_path = '/home/iamshri/Documents/Dataset/p01/CAM_LL/landmarks/BIAH_BS.hdf5'
-    right_hdf5_path = '/home/iamshri/Documents/Dataset/p01/CAM_LL/landmarks/BIAH_BS.hdf5'
-    output_hdf5_path = 'output.hdf5'
-
-    triangulator = TriangulateViews(left_hdf5_path, right_hdf5_path, output_hdf5_path, logger)
-    triangulator.triangulate()
+    triangulate_views = TriangulateViews(stereo_path, left_path, right_path, output_file)
+    triangulate_views.save_3d_points_hdf5()
